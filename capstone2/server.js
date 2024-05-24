d06f415e9f6405c68bd2b201c1f2c97d48c4d698
Pardaaa <rae.hutagalung@gmail.com>
2024-05-24 11:04:59 +0700

    update template

--- capstone2/server.js
+++ capstone2/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import path from "path";
 import ejs from "ejs";
 import { fileURLToPath } from "url";
+import expressLayouts from "express-ejs-layouts";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -13,10 +14,14 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(expressLayouts);
+app.set('layout', 'layouts/master');
 
 // Rute untuk halaman user
 app.get('/user', (req, res) => {
-    res.render('layouts/master');
+    res.render('index');
 });
 
 const port = 8000
